8430b49d90e82d23a80102f5aef684ca568b7f7a
Miguel Angel Vazquez Diaz <polliyo67@gmail.com>
2023-06-15 04:44:04 -0600

    subir apk

---
+++ Primer-Parcial/android/app/src/main/java/tienda/com/app/MainActivity.java
@@ -0,0 +1,5 @@
+package tienda.com.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- Primer-Parcial/src/environments/environment.prod.ts
+++ Primer-Parcial/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyB36MnwZ8vuzFjJj-zc_QoXCunvte4Xl1c",
+    authDomain: "ago-bf7d9.firebaseapp.com",
+    projectId: "ago-bf7d9",
+    storageBucket: "ago-bf7d9.appspot.com",
+    messagingSenderId: "679412670703",
+    appId: "1:679412670703:web:84c09247f7d616bb963b1a",
+    measurementId: "G-NJ8DJY6BQ4"
+  }
 };
